d1fb3e0b7ccbe9eb7d3cc4a07dcf3b0a657ef03f
AHN JAE-HA <eu81273@gmail.com>
2016-04-06 17:50:30 +0900

    first commit :iphone:

---
+++ index.android.js
@@ -0,0 +1,6 @@
+'use strict';
+
+import React, { AppRegistry } from 'react-native';
+import ReactNativeSteps from './01_helloworld';
+
+AppRegistry.registerComponent('ReactNativeSteps', () => ReactNativeSteps);
